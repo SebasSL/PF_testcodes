17dd6869f31d2876818b0ae0aec3cc6b16cfc59c
Sebasmeh <sebslaurens@gmail.com>
2017-11-11 17:28:24 -0500

    meh2

--- frames/color_run.py
+++ frames/color_run.py
@@ -11,8 +11,8 @@ def color_chase(frame):
     
     img=frame.copy()
     img_hsv=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    low=np.array([100,50,30])
-    up=np.array([140,255,255])
+    low=np.array([110,50,30])
+    up=np.array([130,255,255])
     mask=cv2.inRange(img_hsv,low,up)
     out=cv2.bitwise_and(frame,frame,mask=mask)
     out=cv2.erode(out,sq2,iterations=1)
@@ -23,30 +23,40 @@ def draw(frame, out):
     try:
         im=out.copy()
         imgray = cv2.cvtColor(im,cv2.COLOR_BGR2GRAY)
-        ret,thresh = cv2.threshold(imgray,10,255,0)
+        ret,thresh = cv2.threshold(imgray,20,255,0)
         _, contours, _ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        x1=640
-        y1=360
-        x2=0
-        y2=0
         a=[]
         b=[a.append(cnt) for cnt in contours]
-        for i in range(len(a[0])):
-            if a[0][i][0][0]<x1:
-                x1=a[0][i][0][0]
-            if a[0][i][0][1]<y1:
-                y1=a[0][i][0][1]
-            if a[0][i][0][0]>x2:
-                x2=a[0][i][0][0]
-            if a[0][i][0][1]>y2:
-                y2=a[0][i][0][1]
-        cv2.rectangle(frame,(x1,y1),(x2,y2),(0,255,0),2);
+        x1=640*np.ones((1,len(a)))
+        y1=360*np.ones((1,len(a)))
+        x2=np.zeros((1,len(a)))
+        y2=np.zeros((1,len(a)))
+        area = 0
+        for i in range(len(a)):
+            for j in range(len(a[i])):
+                if a[i][j][0][0]<x1[i]:
+                    x1[i]=a[i][j][0][0]
+                if a[i][j][0][1]<y1[i]:
+                    y1[i]=a[i][j][0][1]
+                if a[i][j][0][0]>x2[i]:
+                    x2[i]=a[i][j][0][0]
+                if a[i][j][0][1]>y2[i]:
+                    y2[i]=a[i][j][0][1]
+            
+            if area < (x2[i]-x1[i])*(y2[i]-y1[i]):
+                xi = x1[i]
+                yi = y1[i]
+                xf = x2[i]
+                yf = y2[i]
+                area = (x2[i]-x1[i])*(y2[i]-y1[i])
+        
+        cv2.rectangle(frame,(xi,yi),(xf,yf),(0,255,0),2);
     except:
         pass
 
 #vs = PiVideoStream().start()
 
-arduino= serial.Serial('/dev/ttyUSB0',115200)
+arduino= serial.Serial('/dev/ttyACM0',115200)
 i=0
 dirname=str(time.time())
 os.makedirs(dirname)
@@ -62,13 +72,16 @@ while i < 10:
     frame = cv2.imread("/dev/shm/mjpeg/cam.jpg",cv2.IMREAD_COLOR)
     orig = frame.copy()
     blue = color_chase(frame)
-    draw(frame,blue)
+    area = draw(frame,blue)
     print(i)
     name = "image "+str(i)
     cv2.imwrite(name+".jpg",frame)
     cv2.imwrite(name+" blue.jpg",orig)
     os.rename("/home/pi/Codes/frames/"+name+".jpg", "/home/pi/Codes/frames/"+dirname+"/"+name+".jpg")
     os.rename("/home/pi/Codes/frames/"+name+" blue.jpg", "/home/pi/Codes/frames/"+dirname+"/"+name+" blue.jpg")
+    if area > 0:
+    	os.system('sudo scp -i /home/pi/.ssh/MyKeyPair.pem  /dev/shm/mjpeg/cam.jpg  ubuntu@35.161.176.110:/var/www/html/DORA-E')
+    	i = 10
     time.sleep(1)
     
     print(i)
